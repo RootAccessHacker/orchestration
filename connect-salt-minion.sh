ed31e89a2e41c1b141a340b2e8bca1d64f2f4049
Cerberus <myrmidon@protonmail.ch>
2021-06-29 19:17:06 +0200

    Improved script
    Specific configuration choices added, corrected if-statement;

--- connect-salt-minion.sh
+++ connect-salt-minion.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-echo "Enter the minions' account name"
-read -r account_name
-
-echo "Enter the minions' IP-address"
-read -r ip
+echo -e "\nWhat will the node be doing?\n[1] Run a Wordpress instance\n[2] Run a Docker instance\n[Default] The target will only get default configuration.\n"
+read -r choice
 
 echo "Enter the minions' hostname"
 read -r host_name
 
+echo "Enter the minions' IP-address"
+read -r ip
+
+echo "Enter the minions' account name"
+read -r account_name
+
 # RCE initiate saltstack install + point to master in config
+echo "Sending payload to minion"
 ssh -t $account_name@$ip "sudo apt install curl; curl -L https://bootstrap.saltstack.com -o install_salt.sh ~/; sudo sh ~/install_salt.sh; sudo sed -i \"s/#master: salt/master: 192.168.90.131/\" /etc/salt/minion; sudo systemctl restart salt-minion"
 
 sleep 10
@@ -33,6 +37,18 @@ echo -e "
 sleep 8
 sudo systemctl restart apache2
 
-echo -e "\nConfiguration completed, have a nice day\n"
+echo -e "\nBasic configuration completed\n"
+
+sleep 5
 
 
+# More specific configuration
+if [ $choice == "1" ]; then
+    sudo salt "$host_name" state.apply deploy-wordpress
+
+elif [ $choice == "2" ]; then
+    sudo salt "$host_name" state.apply deploy-docker
+else 
+    echo "Exiting."
+    exit
+fi
